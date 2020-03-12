a7cc98b0031153be16f91f996f879997719fbe9d
Bikash Guragai <bikashguragai@gmail.com>
2020-03-12 13:56:54 -0400

    RangeIter has been added
    RangeIter Added

---
+++ src/com/guragai/RangeIter.java
@@ -0,0 +1,61 @@
+package com.guragai;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class RangeIter implements Iterable<Integer> {
+
+    private int start,end;
+
+    public RangeIter(int start, int end){
+        this.start = start;
+        this.end = end;
+    }
+
+    public Iterator<Integer> iterator(){
+        return new RangeIterator();
+    }
+
+    // Inner class example
+    private class  RangeIterator implements Iterator<Integer>{
+        private int cursor;
+        public RangeIterator(){
+            this.cursor = RangeIter.this.start;
+        }
+
+        public boolean hasNext(){
+            return this.cursor < RangeIter.this.end;
+        }
+
+        public Integer next(){
+            if(this.hasNext()){
+                int current = cursor;
+                cursor++;
+                return current;
+            }
+            throw new NoSuchElementException();
+        }
+
+        public void remove(){
+            throw new UnsupportedOperationException();
+        }
+    }
+
+    public static void main(String[] args) {
+        RangeIter range = new RangeIter(1,10);
+        Iterator<Integer> it = range.iterator();
+
+        //Long way
+//        while(it.hasNext()){
+//            int cur = it.next();
+//            System.out.println(cur);
+//        }
+
+        //Short way
+        for(Integer cur : range){
+            System.out.println(cur);
+        }
+    }
+
+
+}
